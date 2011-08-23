7a23d8d8b00a4f8483b0b1c228848ce886bf292b
chromer <chromer@in.com>
2011-08-23 09:27:14 -0400

    first commit

---
+++ blogs/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Blog(models.Model):
+    title = models.CharField(max_length=128)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User)
