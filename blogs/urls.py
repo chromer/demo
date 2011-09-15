04fa902339fc5e184bdccf02315dd1d662db6b4f
chromer <chromer@in.com>
2011-09-15 07:03:00 -0400

    added blog form

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^$', 'demo.blogs.views.blogs'),
+    (r'^add/$', 'demo.blogs.views.add_blog'),
 )
--- blogs/views.py
+++ blogs/views.py
@@ -1,10 +1,25 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponseRedirect, HttpResponse, Http404
+
 
 from models import Blog
+from forms import BlogForm
 
 def blogs(request):
     blogs = Blog.objects.order_by('-timestamp')
-    return render_to_response('blogs/blog_list.html', RequestContext(request, {'blogs':blogs,}))
+    form = BlogForm()
+    return render_to_response('blogs/blog_list.html', RequestContext(request, {'blogs':blogs, 'blog_form':form}))
+
+def add_blog(request):
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
+            return HttpResponseRedirect('/blogs')
+        else:
+            return HttpResponse('Some error occured please try again later.')
 
